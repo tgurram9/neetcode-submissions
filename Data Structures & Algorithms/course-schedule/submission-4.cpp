92be3b523e7ba7c0354c9a72396c085b0dc21927
Tejeshwar <tejeshwarreddy1996@gmail.com>
2026-09-17 22:33:06 -0700

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegrees(numCourses, 0);
+        vector<vector<int>> adjList(numCourses);
+
+        for (auto p : prerequisites) {
+            adjList[p[1]].push_back(p[0]);
+            indegrees[p[0]]++;
+        }
+        
+        queue<int> q;
+        int finished = 0;
+        for (int i=0; i<indegrees.size(); i++) {
+            if (indegrees[i] == 0) {
+                q.push(i);
+                finished++;
+                indegrees[i] = -1;
+            }
+        }
+
+        while(!q.empty()) {
+            int val = q.front();
+            q.pop();
+
+            auto vec = adjList[val];
+            for (auto it : vec) {
+                indegrees[it]--;
+            }
+
+            for (int i=0; i<indegrees.size(); i++) {
+                if (indegrees[i] == 0) {
+                    q.push(i);
+                    finished++;
+                    indegrees[i] = -1;
+                }
+            }
+        }
+
+        return finished == numCourses;
+    }
+};
